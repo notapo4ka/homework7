8db4188b586ed4bdfc4d6d7a70d700ed37742818
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-12 00:41:28 +0200

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -1,13 +1,24 @@
-public class Cat extends Animals {
+import java.util.Objects;
 
+public class Cat extends Animal {
+    private static int catsCount;
     private final int maxRunMeters = 200;
 
     public Cat(String name) {
         super(name);
+        catsCount++;
+    }
+
+    public static int getCatsCount() {
+        return catsCount;
     }
 
     @Override
     public String run(int distance) {
+        Objects.requireNonNull(distance, "distance must not be null");
+        if (distance == 0) {
+            return "Distance must not be null";
+        }
         if (distance > maxRunMeters) {
             return this.getName() + " не може пробігти так багато";
         }
@@ -16,6 +27,10 @@ public class Cat extends Animals {
 
     @Override
     public String swim(int distance) {
+        Objects.requireNonNull(distance, "distance must not be null");
+        if (distance == 0) {
+            return "Distance must not be null";
+        }
         return this.getName() + " не вміє плавати";
     }
 }
