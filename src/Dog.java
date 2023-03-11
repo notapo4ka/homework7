5c6df6a887e532da3574b26a3c9a1b268f842a99
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-12 00:40:53 +0200

    Update Dog.java

--- src/Dog.java
+++ src/Dog.java
@@ -1,14 +1,26 @@
-public class Dog extends Animals {
+import java.util.Objects;
 
+public class Dog extends Animal {
+
+    private static int dogsCount;
     private final int maxRunMeters = 500;
     private final int maxSwimMeters = 10;
 
     public Dog(String name) {
         super(name);
+        dogsCount++;
+    }
+
+    public static int getDogsCount() {
+        return dogsCount;
     }
 
     @Override
     public String run(int distance) {
+        Objects.requireNonNull(distance, "distance must not be null");
+        if (distance == 0) {
+            return "Distance must not be null";
+        }
         if (distance > maxRunMeters) {
             return this.getName() + " не може пробігти так багато";
         }
@@ -17,6 +29,10 @@ public class Dog extends Animals {
 
     @Override
     public String swim(int distance) {
+        Objects.requireNonNull(distance, "distance must not be null");
+        if (distance == 0) {
+            return "Distance must not be null";
+        }
         if (distance > maxSwimMeters) {
             return this.getName() + " не може проплисти так багато";
         }
